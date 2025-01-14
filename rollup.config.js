9917f2f145756bd552eb7d89f8031284961bbbb0
Mikka <cvyl@pm.me>
2025-01-14 10:51:29 +0100

    refactor: simplify Rollup configuration by removing unused plugins and dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import terser from '@rollup/plugin-terser';
-import {base64} from 'rollup-plugin-base64';
-import glob from 'glob';
-import path from 'path';
+import { base64 } from 'rollup-plugin-base64';
 
 export default {
   input: 'src/index.js',
